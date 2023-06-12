94d32ab465107553fa6a0a6dccae4df99e505e0d
Ethorcharl <zenja_riga5@yahoo.com>
2023-06-12 22:39:49 +0300

    Add project files.

---
+++ AngularSuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,35 @@
+﻿using AngularSuperHeroAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularSuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes() 
+        {
+
+            //return new List<SuperHero> {
+            //    new SuperHero
+            //    {
+            //        Name ="Spider Man",
+            //        FirstName = "Peter",
+            //        LastName = "Parker",
+            //        Place = "New York City"
+            //    }
+            //};
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+    }
+}
---
+++ AngularSuperHeroAPI/Program.cs
@@ -0,0 +1,42 @@
+using AngularSuperHeroAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//add for solving problem with CORS. In WithOrigins we can add new links
+builder.Services.AddCors(options=>
+    options.AddPolicy(name:"SuperHeroOrigins",
+    policy => { policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader(); 
+}));
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// for CORS problem
+app.UseCors("SuperHeroOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
